2b6e9ff65a1c6886efbf27b031563c43e5c2e74e
robinsonweng <robinsonweng100@gmail.com>
2021-02-05 16:01:24 +0900

    adding basic const for chip8

---
+++ src/cpu.h
@@ -0,0 +1,17 @@
+#ifndef __CPU
+# define __CPU
+
+#include <stdlib.h>
+
+#define MAX_RAM_SIZE 4096
+#define WINDOW_HIGHT 800
+#define WINDOW_WIDTH 800
+#define MAX_GENERAL_REGISTER 16
+#define MAX_STACK_LEVEL 16
+
+typedef unsigned char uint8_t;
+typedef unsigned short ushort;
+
+
+
+#endif
